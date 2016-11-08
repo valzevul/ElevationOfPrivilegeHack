92e2ab0bf5c53314ab3e16b2778dd3f8d18df7e0
Vadim Drobinin <vadim@drobinin.com>
2016-11-08 18:54:36 +0300

    Fix open method

--- WellcutDemoExtension/AppDelegate.swift
+++ WellcutDemoExtension/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
   
   func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-    openMainPageFromExtension()
+    openDetailedPageFromExtension()
     return true
   }
   
